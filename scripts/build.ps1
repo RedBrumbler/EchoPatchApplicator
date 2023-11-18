b19291a13f437214c8757cdbcfa568000e682b12
RedBrumbler <redbrumbler.official+github@gmail.com>
2023-11-18 18:08:03 +0100

    build improvements and cleanup

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,6 +1,18 @@
-
+Param(
+    [Parameter(Mandatory=$false)]
+    [Switch]$clean
+)
 $root = "${PSScriptRoot}/.."
 $build = "${root}/build"
+
+if ($clean.IsPresent)
+{
+    if (Test-Path -Path ${build})
+    {
+        remove-item ${build} -R
+    }
+}
+
 
 & cmake -G "Ninja" -DCMAKE_BUILD_TYPE="RelWithDebInfo" ${root} -B ${build}
 & cmake --build ${build}
